cd85a62021e328e0543b8662d9658c43a6866c0f
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-20 02:41:40 -0700

    updated my Armenity class

--- models/amenity.py
+++ models/amenity.py
@@ -10,6 +10,7 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
         Base.metadata,
         Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
         Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
+        extend_existing=True
     )
 
 class Amenity(BaseModel, Base):
