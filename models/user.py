3e3140b819b4e49465f3a8fbd7fad84a68cef4dd
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-20 00:14:59 -0700

    Updated 7 & 8

--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from os import getenv
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
@@ -11,3 +13,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship("Place", backref="user", cascade="all, delete-orphan")
