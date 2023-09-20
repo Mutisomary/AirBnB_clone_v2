c2996f8fbfdbc6e81dd27f79dc0ccda8e9aae403
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-20 02:35:16 -0700

    updated Amenity class

--- models/review.py
+++ models/review.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
+""" Review module for the HBNB project """
+from models.base_model import BaseModel
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    place_amenity = Table(
-        "place_amenity",
-        Base.metadata,
-        Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
-        Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
-    )
 
-class Amenity(BaseModel, Base):
-    """ Amenity class to store amenity information """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", secondary=place_amenity, viewonly=False)
+class Review(BaseModel):
+    """ Review classto store review information """
+    place_id = ""
+    user_id = ""
+    text = ""
