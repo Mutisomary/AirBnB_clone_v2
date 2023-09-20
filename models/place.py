2cbeabc5c6e707d97cc77db2d16e04106c3a7615
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-20 02:24:32 -0700

    Added code for task 10

--- models/place.py
+++ models/place.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from os import getenv
 
-class Place(BaseModel, Base):
+class Place(BaseModel):
     """ A place to stay """
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        # For DBStorage
         __tablename__ = "places"
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
@@ -19,3 +20,33 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+
+        # Define the many-to-many relationship with Amenity using place_amenity
+        place_amenity = Table(
+            "place_amenity",
+            Base.metadata,
+            Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
+            Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
+        )
+
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+
+    else:
+        # For FileStorage
+        @property
+        def amenities(self):
+            """ Getter attribute for amenities """
+            from models import storage
+            amenities = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.get("Amenity", amenity_id)
+                if amenity:
+                    amenities.append(amenity)
+            return amenities
+
+        @amenities.setter
+        def amenities(self, obj):
+            """ Setter attribute for amenities """
+            from models.amenity import Amenity
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
