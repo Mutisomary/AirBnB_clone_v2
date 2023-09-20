80fb96e9e5e1993f0629dab54552a64788f699ca
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-20 02:44:58 -0700

    updated place and armrnity class

--- models/place.py
+++ models/place.py
@@ -26,7 +26,7 @@ class Place(BaseModel):
             "place_amenity",
             Base.metadata,
             Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
-            Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
+            Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False),
             extend_existing=True
         )
 
