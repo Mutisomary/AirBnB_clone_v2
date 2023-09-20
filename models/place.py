ecbb336ea37b0dc62163766ec0d064887ee2f443
Mutisomary <marymutso2016@gmail.com>
2023-09-20 03:33:05 -0700

    updating relatioship

--- models/place.py
+++ models/place.py
@@ -7,15 +7,24 @@ from os import getenv
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "places"
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref='place', cascade="all, delete-orphan")
+
+    else:
+        @property
+        def reviews(self):
+            from models import storage
+            review_instances = storage.all("Review").values()
+            return [review for review in review_instances
+                    if review.place_id == self.id]
