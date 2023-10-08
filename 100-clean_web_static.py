bcd09c279bd0f15b3efcbccbd4a3b606ec0edbde
Mutisomary <marymutso2016@gmail.com>
2023-10-07 19:03:39 -0700

    clean up

---
+++ 100-clean_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This fabfile distributes an archive to my web servers
+"""
+
+import os
+from fabric.api import *
+from datetime import datetime
+
+
+env.hosts = ['54.237.21.182', '35.175.104.231']
+env.user = 'ubuntu'
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives of the static files.
+    Args:
+        number (Any): The number of archives to keep.
+    """
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
+    else:
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
