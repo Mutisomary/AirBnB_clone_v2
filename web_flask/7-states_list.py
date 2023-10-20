34d5dcff990df081919737be746ad453d343f437
Mutisomary <marymutso2016@gmail.com>
2023-10-20 07:24:02 -0700

    states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
-
-from flask import Flask, render_template
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a HTML page with a list of all states"""
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    """view function that displays all states currently in storage"""
+    all_states = storage.all("State").values()
+    return render_template('7-states_list.html', all_states=all_states)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """removes the current SQLAlchemy Session"""
+    """function that removes the current SQLAlchemy Session"""
     storage.close()
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
